4abeded09a784e05bca62f95c7dff07a9e19b656
Estefanía Villarreal <villarrealestefania758@gmail.com>
2024-11-11 19:20:35 -0300

    Update truco.py

--- tpi/utils/cantos/truco.py
+++ tpi/utils/cantos/truco.py
@@ -1,1 +1,20 @@
+# Incompleto
 
+import os
+from utils.interfaz.presentacion_mano import (mostrar_mano)
+
+def truco(ronda, jugador, mano, valor_mano, reiniciar):
+    if ronda:
+        os.system("cls")
+        aux = input("Presione cualquier tecla para continuar")
+        print("Turno del jugador", jugador)
+        mostrar_mano(jugador, mano)
+        print("\nSe cantó truco")
+        canto = input('¿Se quiere? ("q" o "nq"): ')
+        if canto == "q":
+            valor_mano = valor_mano + 1
+            os.system("cls")
+        elif canto == "nq":
+            os.system("cls")
+            reiniciar = True
+    return valor_mano, reiniciar
