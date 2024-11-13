3447daf30e7d61caefa4aa39c231d6d28a960ed2
Estefanía Villarreal <villarrealestefania758@gmail.com>
2024-11-13 19:18:02 -0300

    Update repartir.py

--- tpi/utils/interfaz/repartir.py
+++ tpi/utils/interfaz/repartir.py
@@ -3,7 +3,7 @@
 import random
 def repartir():
     PALO = ['Espada', 'Oro', 'Basto', 'Copas']
-    VALOR = ['1', '2', '3', '4', '5', '6', '7', '11', '12']
+    VALOR = ['1', 2, 3, 4, 5, 6, '7', 10, 11, 12]
     mazo_jugador1 = [0] * 3
     mazo_jugador2 = [0] * 3
     for i in range(3):
