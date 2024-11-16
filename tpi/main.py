439a5fb313861c80ac452bd497fb7bbe534c415d
Lucas-Bergara912 <lucasbergara838@gmail.com>
2024-11-15 21:13:48 -0300

    Update main.py

--- tpi/main.py
+++ tpi/main.py
@@ -1,4 +1,3 @@
-# Función truco: no incrementa el valor de la mano y al no querer no se reparten de nuevo las cartas
 # Incompleto
 
 import random
@@ -6,36 +5,42 @@ from utils.interfaz.repartir import (repartir)
 from utils.interfaz.presentacion_mano import (mostrar_mano)
 from utils.interfaz.opciones_usuario import (opciones_jugador)
 
-mesaj1 = [[0 for i in range(1)] for j in range(3)]
-mesaj2 = [[0 for i in range(1)] for j in range(3)]
+mano1 = [0] * 2
+mano2 = [0] * 2
+mano3 = [0] * 2
 
 manoj1, manoj2 = repartir()
 
-ronda = 1
-valor_mano = 1
+valor_ronda = 1
 jugador = 1
 
 puntosj1 = 0
 puntosj2 = 0
 
-reiniciar = False
+reiniciar = False # Hay demasiadas funciones, ¿cómo podemos hacer llegar el valor True hasta acá?
 
-while ronda <= 3 or reiniciar == False:
+while reiniciar == False:
     print("----- Truco en consola -----")
     print("Turno del jugador", jugador)
-    print("Valor de la mano:", valor_mano)
+    print("Valor de la mano:", valor_ronda)
 
     if jugador == 1:
-        print("Puntos:", puntosj1)
+        print("Puntos:", puntosj1) # Incompleto
         mostrar_mano(jugador, manoj1)
-        opciones_jugador(ronda, jugador, manoj1, manoj2, valor_mano, reiniciar)
+        opciones_jugador(ronda, jugador, manoj1, manoj2, valor_ronda, reiniciar)
+        jugador = 2
     if jugador == 2:
         print("Puntos:", puntosj2)
         mostrar_mano(jugador, manoj2)
-        opciones_jugador(ronda, jugador, manoj2, valor_mano, reiniciar)
+        opciones_jugador(ronda, jugador, manoj2, valor_ronda, reiniciar)
+        jugador = 1
 
-    ronda += 1
+    ronda += 1 # Idea para cambiar de ronda (repartir de nuevo)
 
-if reiniciar == True:
+if reiniciar == True: # Intento de reinicio
     manoj1, manoj2 = repartir()
+    mano1 = [0] * 2
+    mano2 = [0] * 2
+    mano3 = [0] * 2
+
     reiniciar = False
