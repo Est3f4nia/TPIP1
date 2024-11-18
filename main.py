eedfad85063b2d5f0c8c4fac7688c3d55fef1ebb
Estefanía Villarreal <villarrealestefania758@gmail.com>
2024-11-18 11:31:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from buscaminas import (jugar)
+from programa import (mainCM)
+from tpi.mainT import (mainT)
 
+print("----- TPI Grupo 5 -----")
+print('1) Buscaminas')
+print('2) Creador de muebles')
+print('3) Truco')
+opcion = int(input('Seleccione una opción: '))
+
+while opcion != 0:
+    if opcion == 1:
+        buscaminas.jugar()
+    elif opcion == 2:
+        programa.mainCM()
+    elif opcion == 3:
+        tpi.mainT.mainT()
+    else:
+        print("Opción inválida")
