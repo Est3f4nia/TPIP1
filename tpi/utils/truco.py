fde3ca636099ba4ad0094be02a42dd8ff86abf0e
Lucas-Bergara912 <lucasbergara838@gmail.com>
2024-11-18 01:13:05 -0300

    Update truco.py

--- tpi/utils/truco.py
+++ tpi/utils/truco.py
@@ -1,1 +1,33 @@
+import os
+from utils.interfaz import presentacion_mano
+
+
+def truco(ronda, jugador, mano, valor_mano, reiniciar):
+    if ronda:
+        os.system("cls")
+        print("Cambio de jugador")
+        input("Presione enter para continuar")
+        print("Turno del jugador", jugador)
+        presentacion_mano.mostrar_mano(jugador, mano)
+        print("\nSe cantó el petiso")
+        if valor_mano == 1:
+            print("Se cantó: Truco")
+        elif valor_mano == 2:
+            print("Se cantó: Retruco")
+        elif valor_mano == 3:
+            print("Se cantó: Vale Cuatro")
+        canto = input('¿Se quiere? ("q" o "nq"): ')
+
+        if canto == "q":
+            valor_mano = valor_mano + 1
+            os.system("cls")
+        
+        elif canto == "nq":
+            os.system("cls")
+            reiniciar = True
+        
+        ultimo_en_cantar = jugador
+            
+        
+    return valor_mano, reiniciar, ultimo_en_cantar
 
