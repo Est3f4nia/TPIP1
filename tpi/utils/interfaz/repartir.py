3f88a0c28154c4d6383848be3142fa959f16ad07
Lucas-Bergara912 <lucasbergara838@gmail.com>
2024-11-18 01:17:07 -0300

    Update repartir.py

--- tpi/utils/interfaz/repartir.py
+++ tpi/utils/interfaz/repartir.py
@@ -1,29 +1,22 @@
 # Maravilla de la programación
 
 import random
+
+
 def repartir():
-    PALO = ['Espada', 'Oro', 'Basto', 'Copas']
+    PALO = ['Espada', 'Oro', 'Basto', 'Copa']
     VALOR = ['1', 2, 3, 4, 5, 6, '7', 10, 11, 12]
     mazo_jugador1 = [0] * 3
     mazo_jugador2 = [0] * 3
-    for i in range(3):
-        p = random.choice(PALO)
-        n = random.choice(VALOR)
-        carta = n, p
-        if carta not in mazo_jugador1:
-            mazo_jugador1[i] = n, p
-        else:
+    for i in range(6):
+        while True:
             p = random.choice(PALO)
             n = random.choice(VALOR)
             carta = n, p
-    
-        p2 = random.choice(PALO)
-        n2 = random.choice(VALOR)
-        carta2 = n2, p2
-        if carta2 not in mazo_jugador1 and carta2 not in mazo_jugador2:
-            mazo_jugador2[i] = n2, p2
+            if carta not in mazo_jugador1 and carta not in mazo_jugador2:
+                break
+        if i < 3:
+            mazo_jugador1[i] = n, p
         else:
-            p2 = random.choice(PALO)
-            n2 = random.choice(VALOR)
-            mazo_jugador2[i] = n2, p2
+            mazo_jugador2[i-3] = n, p
     return mazo_jugador1, mazo_jugador2
